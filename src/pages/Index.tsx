f4ed50fc49f54a2a63f2a1531e123fa8319c70ed
shoukokomi8008 <shoukokomi8008@gmail.com>
2026-01-20 14:26:25 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -59,7 +59,7 @@ const referrals = [
       "Safe & secure banking",
       "UPI payments enabled",
     ],
-    link: "https://i.airtel.in/refandearnLGXDMKCKYC",
+    link: "https://i.airtel.in/refandearnPJXQTLCKYC",
     logo: airtelLogo,
     brandColor: "#ED1C24",
   },
